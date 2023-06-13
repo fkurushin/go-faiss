2c2e6482aa8267baee35674b807e7022e166fb18
fkurushin <f.kurushin@ya.ru>
2023-06-13 10:32:37 +0300

    add more tests

--- faiss_test.go
+++ faiss_test.go
@@ -1,6 +1,7 @@
 package faiss
 
 import (
+	"strconv"
 	"testing"
 )
 
@@ -8,5 +9,23 @@ func TestReadIndex(t *testing.T) {
 	_, err := ReadIndex("/Users/fedorkurusin/Desktop/presets.index", IOFlagReadOnly)
 	if err != nil {
 		t.Errorf("can't read index: %v", err)
+	}
+}
+
+func TestIndexFactory(t *testing.T) {
+	d := 100
+	k := 1000
+	_, err := IndexFactory(d, "IVF"+strconv.Itoa(k)+",Flat", MetricInnerProduct)
+	if err != nil {
+		t.Errorf("failed to create FAISS index: %v", err)
+	}
+}
+
+func TestTrain(t *testing.T) {
+	d := 100
+	k := 1000
+	_, err := IndexFactory(d, "IVF"+strconv.Itoa(k)+",Flat", MetricInnerProduct)
+	if err != nil {
+		t.Errorf("failed to create FAISS index: %v", err)
 	}
 }
